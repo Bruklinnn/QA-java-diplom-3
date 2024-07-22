128407a917c13fd55950c27c6deba15bd3e49bd3
Bruklinnn <167115794+Bruklinnn@users.noreply.github.com>
2024-07-22 18:13:01 +0300

    Готово 3е задание

--- src/main/java/PageObject/MainPage.java
+++ src/main/java/PageObject/MainPage.java
@@ -1,77 +1,125 @@
-package MainPaige;
+package PageObject;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import static driver.Links.waitOfVisibleElement;
+
 public class MainPage {
     private final WebDriver driver;
-    private static final String URL = "https://stellarburgers.nomoreparties.site/";
+    private static final String MAIN_URL = "https://stellarburgers.nomoreparties.site/";
     private final By stellarBurgers = By.xpath(".//div[@class ='AppHeader_header__logo__2D0X2'])");
     private final By OrdersLent = By.xpath(".//a[@class ='AppHeader_header__link__3D_hX AppHeader_header__link_active__1IkJo']"); /*Лента заказов*/
     private final By personalAccount = By.xpath(".//p[text()='Личный Кабинет']"); /*Личный кабинет*/
     private final By constructor = By.xpath(".//p[text()='Конструктор']"); /*Конструктор*/
-    private final By LogInMain = By.xpath(".//Button[@class = 'button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_large__G21Vg']"); /*Кнопка войти в аккаунт на главной*/
-    private final By Buns = By.xpath(".//div[@class = 'tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']"); /*Булки*/
-    private final By Sauces = By.xpath(".//div[@class = 'tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect']"); /*Соусы*/
-    private final By Fillings = By.xpath(".//div[@class = 'tab_tab__1SPyG  pt-4 pr-10 pb-4 pl-10 noselect']"); /*Начинки*/
-    private final By LogInPersonalAccount = By.xpath(".//Button[@class = 'button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa']"); /*Кнопка войти в аккаунт в личном кабинете*/
-    private final By LogInRegistration = By.xpath(".//a[@class = 'Auth_link__1fOlj']"); /*Кнопка войти на странице регистрации*/
-    private final By Registration = By.xpath(".//button[@class = 'button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa']"); /*Кнопка зарегестрироваться*/
+    private final By LogInMain = By.xpath(".//button[text() = 'Войти в аккаунт']"); /*Кнопка войти в аккаунт на главной*/
+    private final By createOrderButton = By.xpath(".//button[text() = 'Оформить заказ']"); /*Кнопка оформить заказ(появляется, когда залогинишься)*/
+    private final By Buns = By.xpath(".//span[text() = 'Булки']/parent::div");; /*Булки*/
+    private final By Sauces = By.xpath(".//span[text() = 'Соусы']"); /*Соусы*/
+    private final By Fillings = By.xpath(".//span[text() = 'Начинки']/parent::div"); /*Начинки*/
+    private final By activeTab = By.xpath(".//div[contains(@class, 'current')]");
+    private final By mainTextConstructor = By.xpath(".//*[text() = 'Соберите бургер']");
+
 
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
+    @Step
     public void open() {
-        driver.get(URL);
+        driver.get(MAIN_URL);
     }
 
-    public void clickStellarBurgers(){
-        driver.findElement(stellarBurgers).click();
-    }
-    public void clickOrdersLent(){
-        driver.findElement(OrdersLent).click();
+    @Step
+            ("Login via loginAccountButton")
+    public void clickLoginAccountButton() {
+        driver.findElement(LogInMain).click();
     }
 
-    public void personalAccount(){
+    @Step
+            ("Login via accountProfileButton")
+    public void clickAccountProfileButton() {
         driver.findElement(personalAccount).click();
     }
 
-    public void constructor(){
-        driver.findElement(constructor).click();
+    @Step("Get create order button text")
+    public String getCreateOrderButtonText() {
+        return driver.findElement(createOrderButton).getText();
     }
-    public void Buns(){
+
+    @Step
+            ("Pick bun")
+    public void clickBuns() {
         driver.findElement(Buns).click();
     }
-    public void Sauces(){
+
+    @Step
+            ("Get buns text")
+    public String getBunsText() {
+        return driver.findElement(Buns).getText();
+    }
+
+    @Step
+            ("Pick sauces")
+    public void clickSauces() {
         driver.findElement(Sauces).click();
     }
-    public void Fillings(){
+
+    @Step
+            ("Get sauces text")
+    public String getSaucesText() {
+        return driver.findElement(Sauces).getText();
+    }
+
+    @Step
+            ("Pick fillings")
+    public void clickFillings() {
         driver.findElement(Fillings).click();
     }
-    public void LogInPersonalAccount(){
-        driver.findElement(LogInPersonalAccount).click();
-    }
-    public void LogInRegistration(){
-        driver.findElement(LogInRegistration).click();
-    }
-    public void Registration(){
-        driver.findElement(Registration).click();
+
+    @Step
+            ("Get fillings text")
+    public String getFillingsText() {
+        return driver.findElement(Fillings).getText();
     }
 
-
-
-    public void SkrollToAboutImportantThings(){
-        WebElement QuestionsAboutImportantThings = driver.findElement(By.xpath(".//div[text()='Вопросы о важном']"));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",QuestionsAboutImportantThings);
+    @Step
+            ("Get active tab text")
+    public String getActiveTabText() {
+        return driver.findElement(activeTab).getText();
     }
 
+    @Step
+            ("Is bun tab selected?")
+    public boolean isBunsSelected() {
+        return getBunsText().equals(getActiveTabText());
+    }
 
-    public void clickMkad(){
-        driver.findElement(MKAD).click();
-        String text = driver.findElement(By.id("accordion__panel-7")).getText();
+    @Step
+            ("Is sauce tab selected?")
+    public boolean isSaucesSelected() {
+        return getSaucesText().equals(getActiveTabText());
+    }
+
+    @Step
+            ("Is filling tab selected?")
+    public boolean isFillingsSelected() {
+        return getFillingsText().equals(getActiveTabText());
+    }
+
+    @Step
+            ("Get main text")
+    public String getMainTextConstructor() {
+        return driver.findElement(mainTextConstructor).getText();
+    }
+
+    @Step
+            ("Wait for visible of createOrderButton")
+    public void waitCreateOrderButton() {
+        waitOfVisibleElement(driver, createOrderButton);
     }
 }
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,2 +1,123 @@
-package PACKAGE_NAME;public class LoginTest {
-}
+import AllForUser.RegUser;
+import AllForUser.User;
+import AllForUser.UserSteps;
+import PageObject.*;
+import driver.WebDriverCreator;
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.response.Response;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class LoginTest {
+    private WebDriver driver;
+    private PersonalAccountPage personalAccountPage;
+    private MainPage mainPage;
+    private LoginPage loginPage;
+    private RegistrationPage registrationPage;
+    private ForgotPasswordPage forgotPasswordPage;
+    private static User userLogin;
+    private static User userReg;
+    private static UserSteps userSteps;
+    private RegUser regUser;
+    private boolean userCreated;
+
+
+    @Before
+    public void setUp() {
+        driver = WebDriverCreator.createWebDriver();
+        loginPage = new LoginPage(driver);
+        mainPage = new MainPage(driver);
+        registrationPage = new RegistrationPage(driver);
+        forgotPasswordPage = new ForgotPasswordPage(driver);
+        userCreated = false;
+        userLogin = new User("Reg@mail.ru", "Reg1233");
+        userReg = new User("Reg@mail.ru", "Reg1233", "Reg");
+        UserSteps.createUser(userReg, regUser);
+
+    }
+
+    @Test
+    @DisplayName("Login user via loginAccountButton")
+    public void loginUserViaLoginAccountButton() {
+        mainPage.open();
+        mainPage.clickLoginAccountButton();
+        loginPage.loginUser(userLogin.getEmail(), userLogin.getPassword());
+        loginPage.waitEnterButtonDisplayed();
+        loginPage.clickEnterButton();
+        mainPage.waitCreateOrderButton();
+        String expected = "Оформить заказ";
+        String actual = mainPage.getCreateOrderButtonText();
+        assertEquals(expected, actual);
+        userCreated = true;
+    }
+
+    @Test
+    @DisplayName("Login user via accountProfileButton")
+    public void loginUserViaAccountProfileButton() {
+        mainPage.open();
+        mainPage.clickAccountProfileButton();
+        loginPage.loginUser(userLogin.getEmail(), userLogin.getPassword());
+        loginPage.waitEnterButtonDisplayed();
+        loginPage.clickEnterButton();
+        mainPage.waitCreateOrderButton();
+        String expected = "Оформить заказ";
+        String actual = mainPage.getCreateOrderButtonText();
+        assertEquals(expected, actual);
+        userCreated = true;
+    }
+
+    @Test
+    @DisplayName("Login user via RegistrationPage")
+    public void loginUserViaRegistrationPage() {
+        mainPage.open();
+        mainPage.clickLoginAccountButton();
+        loginPage.loginUser(userLogin.getEmail(), userLogin.getPassword());
+        loginPage.waitEnterButtonDisplayed();
+        loginPage.clickEnterButton();
+        mainPage.waitCreateOrderButton();
+        String expected = "Оформить заказ";
+        String actual = mainPage.getCreateOrderButtonText();
+
+        assertEquals(expected, actual);
+        userCreated = true;
+    }
+
+    @Test
+    @DisplayName("Login user via ForgotPasswordPage")
+    public void loginViaForgotPasswordPage() {
+        forgotPasswordPage.openForgotPasswordPage();
+        forgotPasswordPage.clickLoginButton();
+        loginPage.loginUser(userLogin.getEmail(), userLogin.getPassword());
+        loginPage.waitEnterButtonDisplayed();
+        loginPage.clickEnterButton();
+        mainPage.waitCreateOrderButton();
+        String expected = "Оформить заказ";
+        String actual = mainPage.getCreateOrderButtonText();
+
+        assertEquals(expected, actual);
+        userCreated = true;
+    }
+
+    @After
+
+    public void tearDown() {
+        if (userCreated) {
+            String accessToken = UserSteps.getAccessToken(userLogin);
+            if (accessToken != null) UserSteps.deleteUser(userLogin, regUser, accessToken);
+            driver.quit();
+        }
+    }
+    }
